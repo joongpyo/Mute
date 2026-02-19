68ee0c55a4a338bb0ee77f8dc4e885ce9f234b1c
joongpyo <tlswndvy@gmail.com>
2026-02-19 20:28:22 +0900

    Initial Commit

---
+++ Mute/MuteApp.swift
@@ -0,0 +1,32 @@
+//
+//  MuteApp.swift
+//  Mute
+//
+//  Created by JP on 2/19/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MuteApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
